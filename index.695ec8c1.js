1f5d8f6f963508e1eb51b197aa1afba0be870a38
Fstv-Electro <104622826+Fstv-Electro@users.noreply.github.com>
2022-10-17 11:05:07 +0000

    Deploying to gh-pages from @ Fstv-Electro/donut-project@6d963606fe52938968077aa20d0ef4ee17db5208 🚀

--- index.695ec8c1.js
+++
@@ -1,2 +0,0 @@
-function readMoreFunc(){var e=document.getElementById("read-more"),n=document.getElementById("myBtn");"none"==e.style.display?(e.style.display="inline",n.innerHTML="Less"):(n.innerHTML="Read more",e.style.display="none")}
-//# sourceMappingURL=index.695ec8c1.js.map
---
+++ index.6cad1b6c.js
@@ -0,0 +1,2 @@
+function readMoreFunc(){var e=document.getElementById("read-more"),n=document.getElementById("myBtn");"none"==e.style.display?(e.style.display="inline",n.innerHTML="Less"):(n.innerHTML="Read more",e.style.display="none")}function readMoreProgramFunc(){var e=document.getElementById("more"),n=document.getElementById("program_btn");"none"==e.style.display?(e.style.display="inline",n.innerHTML="Less"):(n.innerHTML="Read more",e.style.display="none")}
+//# sourceMappingURL=index.6cad1b6c.js.map
