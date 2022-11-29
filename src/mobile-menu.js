da1b7f49e096ec703399d5973c13aa67416c0f74
Oleksandr Kutsenko <104622826+Fstv-Electro@users.noreply.github.com>
2022-11-29 15:29:23 +0200

    last fix bugs

--- src/mobile-menu.js
+++ src/mobile-menu.js
@@ -3,10 +3,12 @@
     openMenuBtn: document.querySelector('[data-menu-open]'),
     closeMenuBtn: document.querySelector('[data-menu-close]'),
     menu: document.querySelector('[data-menu]'),
+    mobileMenu: document.querySelector('.mobile-menu-list'),
   };
 
   refs.openMenuBtn.addEventListener('click', toggleModal);
   refs.closeMenuBtn.addEventListener('click', toggleModal);
+  refs.mobileMenu.addEventListener('click', toggleModal);
 
   function toggleModal() {
     refs.menu.classList.toggle('is-open');
--- src/read-more.js
+++ src/read-more.js
@@ -1,37 +1,34 @@
 function readMoreFunc() {
-var moreText = document.getElementById("read-more");
-var btnText = document.getElementById("myBtn");
+  var moreText = document.getElementById('read-more');
+  var btnText = document.getElementById('myBtn');
 
-    if (moreText.style.display == "none") {
-        moreText.style.display = "inline";
-        btnText.innerHTML = "Less";
-    } else {
-        btnText.innerHTML = "Read more";
-        moreText.style.display = "none";
-    }
+  if (moreText.style.display == 'none') {
+    moreText.style.display = 'inline';
+    btnText.innerHTML = 'Less';
+  } else {
+    btnText.innerHTML = 'Read more';
+    moreText.style.display = 'none';
+  }
 }
-
 
 // КНОПКА READ MORE PROGRAM SECTION
 
-
-
 function readMoreProgramFunc() {
-    // Параграф, спан чи те що буде всередині id="more" буде приховуватися
-    var moreText = document.getElementById("more");  
-    // Твоя кнопка з індексом:
-    // <button onclick="readMoreProgramFunc()" class="program__btn" id="program_btn">
-    //   Read more
-    // </button>
-var btnText = document.getElementById("program_btn");
+  // Параграф, спан чи те що буде всередині id="more" буде приховуватися
+  var moreText = document.getElementById('more');
+  // Твоя кнопка з індексом:
+  // <button onclick="readMoreProgramFunc()" class="program__btn" id="program_btn">
+  //   Read more
+  // </button>
+  var btnText = document.getElementById('program_btn');
 
-    if (moreText.style.display == "none") {
-        moreText.style.display = "inline";
-        btnText.innerHTML = "Less";
-    } else {
-        btnText.innerHTML = "Read more";
-        moreText.style.display = "none";
-    }
+  if (moreText.style.display == 'none') {
+    moreText.style.display = 'inline';
+    btnText.innerHTML = 'Less';
+  } else {
+    btnText.innerHTML = 'Read more';
+    moreText.style.display = 'none';
+  }
 }
 // В scss додай:
 // #more {
